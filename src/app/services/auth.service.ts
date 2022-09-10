8def65ef6b6bfd5417ebd6465a8ca9a3625c09c7
Ansar Memon <ansarmemon@Ansars-MacBook-Pro.local>
2022-09-09 19:43:59 -0700

    code refactoring

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth'
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import IUser from '../models/user.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(private auth: AngularFireAuth, private db: AngularFirestore) { }
+
+  public async createUser(userData: IUser) {
+    const { name, email, age, phoneNumber, password } = userData;
+
+    const userCred = await this.auth.createUserWithEmailAndPassword(email as string, password as string);
+
+    await this.db.collection('users').add({ name, email, age, phoneNumber });
+  }
+}
