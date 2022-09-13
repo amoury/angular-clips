15d69e561ab974255625053fdab7f0b191cea585
Ansar Memon <ansarmemon@Ansars-MacBook-Pro.local>
2022-09-12 18:04:14 -0700

    Added manage videos page

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { ModalService } from '../services/modal.service';
-import { AngularFireAuth } from '@angular/fire/compat/auth'
 
 @Component({
   selector: 'app-nav',
@@ -9,7 +8,10 @@ import { AngularFireAuth } from '@angular/fire/compat/auth'
   styleUrls: ['./nav.component.css'],
 })
 export class NavComponent implements OnInit {
-  constructor(public modal: ModalService, public auth: AuthService, public afAuth: AngularFireAuth) { }
+  constructor(
+    public modal: ModalService,
+    public auth: AuthService,
+  ) { }
 
   ngOnInit(): void {
   }
@@ -17,11 +19,6 @@ export class NavComponent implements OnInit {
   openModal($event: Event) {
     $event.preventDefault();
     this.modal.toggleModal('auth');
-  }
-
-  async logout($event: Event) {
-    $event.preventDefault();
-    await this.afAuth.signOut();
   }
 
 }
--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/compat/auth'
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/compat/firestore';
 import { map, Observable, delay } from 'rxjs';
+import { Router } from '@angular/router';
 import IUser from '../models/user.model';
 
 @Injectable({
@@ -12,8 +13,12 @@ export class AuthService {
   public isAuthenticated$: Observable<boolean>;
   public isAuthenticatedWithDelay$: Observable<boolean>;
 
-  constructor(private auth: AngularFireAuth, private db: AngularFirestore) {
-    this.usersCollection = db.collection('users');
+  constructor(
+    private auth: AngularFireAuth,
+    private db: AngularFirestore,
+    private router: Router
+  ) {
+    this.usersCollection = this.db.collection('users');
     this.isAuthenticated$ = auth.user.pipe(map(user => !!user))
     this.isAuthenticatedWithDelay$ = this.isAuthenticated$.pipe(delay(1000))
   }
@@ -32,5 +37,16 @@ export class AuthService {
     await userCred.user.updateProfile({
       displayName: userData.name
     })
+  }
+
+
+  public async logout($event?: Event) {
+    if ($event) {
+      $event.preventDefault();
+    }
+
+    await this.auth.signOut();
+
+    await this.router.navigateByUrl('/')
   }
 }
